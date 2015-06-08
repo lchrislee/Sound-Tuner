c5ad41759c12483fc82cbd14a0f70de3df5de858
Chris Lee <chrisl@usc.edu>
2015-06-08 11:19:37 -0700

    Added more functions to improve readability. Attempted to do more with the requests.

--- Chrome/js/popup.js
+++ Chrome/js/popup.js
@@ -1,55 +1,47 @@
 //alert("hi");
 chrome.tabs.getAllInWindow(showInTabs);
-var limit = 2;
+var limit = 3;
 
 function showInTabs(tabs){
 	var numTabs = tabs.length;
 	var body = document.getElementsByTagName('tbody')[0];
 	for (var i = 0; i < numTabs; i++){
 		if (i == limit){
-			var body = document.getElementsByTagName('tbody')[0];
-			var row = body.insertRow(i);
-			var sub = row.insertCell(0);
-			var button = document.createElement('input');
-			button.setAttribute('type', 'button');
-			button.setAttribute('value', 'More');
-			button.addEventListener("click", function(event){
-				if (event.target.value == 'More'){
-					for (var j = limit; j < numTabs; j++){
-						create(tabs, body, j);
-					}
-					event.target.value = 'Less';
-				}else{
-					for (var j = numTabs; j > limit; --j){
-						destroy(tabs, body, j - 1);
-					}
-					event.target.value = 'More';
-				}
-			});
-			sub.appendChild(button);
+			moreButton(tabs, numTabs, body, i);
 			break;
 		}
 		else{			
-			//create the image
 			create(tabs, body, i);
 		}
-		//check for media
-		chrome.tabs.sendMessage(tabs[i].id, {method: "getHTML", icon: i}, function(response){
-			//alert("some response\n" + response);
-			if (response.method == "getHTML"){
-				alert("response correct");
-				//var list = document.body.getElementsByTagName("p");
-				alert(response.data);
-			}
-		});
 	}
 };
 
-function destroy(tabs, body, i){
-	body.deleteRow(i);
+function moreButton (tabs, numTabs, body, i){
+	var row = body.insertRow(i);
+	var sub = row.insertCell(0);
+	var button = document.createElement('input');
+	button.setAttribute('type', 'button');
+	button.setAttribute('value', 'More');
+	button.addEventListener("click",
+		function(event){
+			if (event.target.value == 'More'){
+				for (var j = limit; j < numTabs; j++){
+					create(tabs, body, j);
+				}
+				event.target.value = 'Less';
+			}else{
+				for (var j = numTabs; j > limit; --j){
+					body.deleteRow(j - 1);
+				}
+				event.target.value = 'More';
+			}
+		}
+	);
+	sub.appendChild(button);
 }
 
 function create(tabs, body, i){
+	//create the image
 	var row = body.insertRow(i);
 	var section = row.insertCell(0);
 	var image = document.createElement('img');
@@ -67,7 +59,19 @@ function create(tabs, body, i){
 	range.setAttribute('defaultValue', '100');
 	range.setAttribute('name', 'rng' + i);
 	range.addEventListener("click", function(event){
-		alert(event.target.name + ": " + event.target.value);
+		//check for media
+		alert(event.target.name + " clicked to " + event.target.value);
+		chrome.tabs.sendMessage(tabs[i].id, {"method": "getHTML", "iconNum": i, "value": event.target.value}, function(response){
+			//alert("some response\n" + response);
+			if (response.method == "getHTML"){
+				if (response.data){
+					var amount = response.data.length;
+					for (var j = 0; j < amount; j++){
+						response.data[j].volume = response.value;
+					}
+				}
+			}
+		});
 	});
 	slide.appendChild(range);
 };
