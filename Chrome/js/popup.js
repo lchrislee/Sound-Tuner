33439882584d41f5426b7872b860bf509710f6f0
Chris Lee <chrisl@usc.edu>
2015-06-08 10:04:36 -0700

    Added a limit to number of sound sliders. Can increase/decrease with a button. Can also detect slider value.

--- Chrome/js/popup.js
+++ Chrome/js/popup.js
@@ -1,28 +1,38 @@
 //alert("hi");
 chrome.tabs.getAllInWindow(showInTabs);
+var limit = 2;
 
 function showInTabs(tabs){
 	var numTabs = tabs.length;
+	var body = document.getElementsByTagName('tbody')[0];
 	for (var i = 0; i < numTabs; i++){
-		var section = document.createElement('div');
-		
-		//create the image
-		var image = document.createElement('img');
-		image.src = tabs[i].favIconUrl;
-		section.appendChild(image);
-		document.body.appendChild(section);
-		
-		//create the slider
-		var range = document.createElement('input');
-		range.setAttribute('type', 'range');
-		range.setAttribute('max', '100');
-		range.setAttribute('min', '0');
-		range.setAttribute('step', '1');
-		range.setAttribute('value', '100');
-		range.setAttribute('defaultValue', '100');
-		range.setAttribute('name', 'sldr' + i);
-		section.appendChild(range);
-		
+		if (i == limit){
+			var body = document.getElementsByTagName('tbody')[0];
+			var row = body.insertRow(i);
+			var sub = row.insertCell(0);
+			var button = document.createElement('input');
+			button.setAttribute('type', 'button');
+			button.setAttribute('value', 'More');
+			button.addEventListener("click", function(event){
+				if (event.target.value == 'More'){
+					for (var j = limit; j < numTabs; j++){
+						create(tabs, body, j);
+					}
+					event.target.value = 'Less';
+				}else{
+					for (var j = numTabs; j > limit; --j){
+						destroy(tabs, body, j - 1);
+					}
+					event.target.value = 'More';
+				}
+			});
+			sub.appendChild(button);
+			break;
+		}
+		else{			
+			//create the image
+			create(tabs, body, i);
+		}
 		//check for media
 		chrome.tabs.sendMessage(tabs[i].id, {method: "getHTML", icon: i}, function(response){
 			//alert("some response\n" + response);
@@ -33,4 +43,31 @@ function showInTabs(tabs){
 			}
 		});
 	}
+};
+
+function destroy(tabs, body, i){
+	body.deleteRow(i);
+}
+
+function create(tabs, body, i){
+	var row = body.insertRow(i);
+	var section = row.insertCell(0);
+	var image = document.createElement('img');
+	image.src = tabs[i].favIconUrl;
+	section.appendChild(image);
+	
+	//create the slider
+	var slide = row.insertCell(1);
+	var range = document.createElement("input");
+	range.setAttribute('type', 'range');
+	range.setAttribute('max', '100');
+	range.setAttribute('min', '0');
+	range.setAttribute('step', '1');
+	range.setAttribute('value', '100');
+	range.setAttribute('defaultValue', '100');
+	range.setAttribute('name', 'rng' + i);
+	range.addEventListener("click", function(event){
+		alert(event.target.name + ": " + event.target.value);
+	});
+	slide.appendChild(range);
 };
