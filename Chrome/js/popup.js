de76b29020d39a3abdaed85a89876f2560a97500
Chris Lee <chrisl@usc.edu>
2015-06-07 20:31:30 -0700

    Dynamic creation of a slider and favicon for each tab.

--- Chrome/js/popup.js
+++ Chrome/js/popup.js
@@ -1,13 +1,31 @@
-chrome.browserAction.onClicked.addListener(editCurrent);
-
-function editCurrent(currentTab){
-	chrome.tabs.getAllInWindow();
-};
+//alert("hi");
+chrome.tabs.getAllInWindow(showInTabs);
 
 function showInTabs(tabs){
 	var numTabs = tabs.length;
 	for (var i = 0; i < numTabs; i++){
-		var section = document.createElement('DIV');
+		//alert(tabs[i].url);
+		var section = document.createElement('p');
+		var image = document.createElement('img');
+		image.src = tabs[i].favIconUrl;
+		section.appendChild(image);
 		document.body.appendChild(section);
+		var range = document.createElement('input');
+		range.setAttribute('type', 'range');
+		range.setAttribute('max', '100');
+		range.setAttribute('min', '0');
+		range.setAttribute('step', '1');
+		range.setAttribute('value', '100');
+		range.setAttribute('defaultValue', '100');
+		range.setAttribute('name', 'sldr' + i);
+		section.appendChild(range);
+		chrome.tabs.sendMessage(tabs[i].id, {method: "getHTML", icon: i}, function(response){
+			//alert("some response\n" + response);
+			if (response.method == "getHTML"){
+				alert("response correct");
+				//var list = document.body.getElementsByTagName("p");
+				alert(response.data);
+			}
+		});
 	}
 };
