f72948bd8d29664cb62611040861a0a1a7318a84
Chris Lee <chrisl@usc.edu>
2015-06-07 17:58:12 -0700

    Starting to create starting elements and tuning manifest.json.

--- Chrome/js/popup.js
+++ Chrome/js/popup.js
@@ -1,1 +1,13 @@
+chrome.browserAction.onClicked.addListener(editCurrent);
 
+function editCurrent(currentTab){
+	chrome.tabs.getAllInWindow();
+};
+
+function showInTabs(tabs){
+	var numTabs = tabs.length;
+	for (var i = 0; i < numTabs; i++){
+		var section = document.createElement('DIV');
+		document.body.appendChild(section);
+	}
+};
