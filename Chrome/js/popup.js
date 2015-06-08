89bd035b3fd1e6fcbeb4817b79435657810a490d
Chris Lee <chrisl@usc.edu>
2015-06-07 21:55:17 -0700

    Added attempts to get content info and align slider with image.

--- Chrome/js/popup.js
+++ Chrome/js/popup.js
@@ -4,12 +4,15 @@ chrome.tabs.getAllInWindow(showInTabs);
 function showInTabs(tabs){
 	var numTabs = tabs.length;
 	for (var i = 0; i < numTabs; i++){
-		//alert(tabs[i].url);
-		var section = document.createElement('p');
+		var section = document.createElement('div');
+		
+		//create the image
 		var image = document.createElement('img');
 		image.src = tabs[i].favIconUrl;
 		section.appendChild(image);
 		document.body.appendChild(section);
+		
+		//create the slider
 		var range = document.createElement('input');
 		range.setAttribute('type', 'range');
 		range.setAttribute('max', '100');
@@ -19,6 +22,8 @@ function showInTabs(tabs){
 		range.setAttribute('defaultValue', '100');
 		range.setAttribute('name', 'sldr' + i);
 		section.appendChild(range);
+		
+		//check for media
 		chrome.tabs.sendMessage(tabs[i].id, {method: "getHTML", icon: i}, function(response){
 			//alert("some response\n" + response);
 			if (response.method == "getHTML"){
